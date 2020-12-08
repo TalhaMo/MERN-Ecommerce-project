ebd5257daa8fc2d8550e1a0e397e1072701a1eea
talhamo <talhamohamed23@yahoo.com>
2020-12-08 17:48:58 +0100

    add build deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const connectDB = require('./config/connectDB')
 const path = require('path')
+const config = require("config");
 
 const app=express()
 
@@ -16,13 +17,21 @@ app.use('/api/upload',require('./routes/uploadRoute'));
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get('/',(req,res)=>{
-    res.send('API is running...')
-})
+if (config.get("NODE_ENV") === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
-const PAYPAL_CLIENT_ID = "AShPbTI-zn7pE34RgILWLa9ziTWFKnt2yubzicsJ6RIsyiwNT1eFgU_VNZGkSDN3Td4XenoatpCT3xj3"
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
+
 app.get('/api/config/paypal', (req, res) =>
-  res.send(PAYPAL_CLIENT_ID)
+  res.send(config.get("PAYPAL_CLIENT_ID"))
   );
 
 const port= process.env.PORT || 5000
