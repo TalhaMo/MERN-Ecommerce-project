6483f738e5034165d10a899c86926e8aa9465db2
talhamo <talhamohamed23@yahoo.com>
2020-12-10 13:16:11 +0100

    add middlware logs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const connectDB = require('./config/connectDB')
 const path = require('path')
+const morgan=require('morgan')
 const config = require("config");
 
 const app=express()
@@ -16,6 +17,10 @@ app.use('/api/upload',require('./routes/uploadRoute'));
 
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 if (config.get("NODE_ENV") === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
